e64d6f72604cfecf21e6934aceaf36bd14efc9ac
anhminh1241 <73695612+anhminh1241@users.noreply.github.com>
2024-04-09 21:01:36 +0700

    test 2
    test 2

--- Server/TestProject1/AccountRepositoryTests.cs
+++ Server/TestProject1/AccountRepositoryTests.cs
@@ -5,7 +5,7 @@ using Project_MLD.Service.Repository;
 using System.Threading.Tasks;
 using System.Linq;
 
-namespace Project_MLD.Tests
+namespace TestProject1
 {
     public class AccountRepositoryTests
     {
