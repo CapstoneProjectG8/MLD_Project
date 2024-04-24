ea9f6e658507ff467f393e895a52893ac17717c0
anhminh1241 <73695612+anhminh1241@users.noreply.github.com>
2024-04-24 22:31:24 +0700

    update menu 4

--- Client/src/components/Layout/SubNavBar/LeftSubNavBar/index.tsx
+++ Client/src/components/Layout/SubNavBar/LeftSubNavBar/index.tsx
@@ -50,12 +50,19 @@ const LeftSubNavBar = () => {
     )
 }
 
+// may th kia , co th nao ranh ko , bao no sua me cai api cho nhanh
+// dang lay submenu o trong document chu dep lam gi 
+
 
 
 const Document = ({ dep }: any) => {
     const [documents, setDocuments] = useState<any>([]);
     const [show, setShow] = useState<any>(false);
-
+    const [rs1,Setrs1] =  useState<any>([]);
+    const [rs2,Setrs2] =  useState<any>([]);
+    const [rs3,Setrs3] =  useState<any>([]);
+    const [rs4,Setrs4] =  useState<any>([]);
+    
     useEffect(() => {
         const getDocument = async () => {
             var doc1 = await axios.get(base_url + "Document1/GetDocument1ByUserSpecialiedDepartment?listId=" + dep.id)
@@ -75,13 +82,22 @@ const Document = ({ dep }: any) => {
                 return {...item , submenu : 4};
             });
             //setDocuments([...doc1.data[1].documents, ...doc2.data[2].documents, ...doc3.data[3].documents, ...doc4.data[4].documents]);
-            setDocuments([...rs1, ...rs2, ...rs3, ...rs4]);
+            // setDocuments([...rs1, ...rs2, ...rs3, ...rs4]);
+            Setrs1(rs1);
+            Setrs2(rs2);
+            Setrs3(rs3);
+            Setrs4(rs4);
         }
         
+        //de luu bien
        
         getDocument();
     }, [dep.id])
 
+  // la hien o dau
+  // vc du lieu o dau ra , voi api the kia
+  // chuan chua
+  // tao component khac roi tuong tu
   
 
     return <>
@@ -89,18 +105,43 @@ const Document = ({ dep }: any) => {
             <Folder style={{ width: "30", height: "30", color: "orange" }} />
             <div>{dep.name}</div>
         </div>
+       
         <div className={show ? 'nav-is-show' : 'nav-is-hidden'}>
-            {documents.map((doc: any) => (
-                <Link key={doc.id} to={`/sub-menu-${doc.submenu}/detail-view/${doc.id}`}>
-                <div className='sub-nav-bar-item-content-folder-course'>
-                    <Article style={{ width: "30px", height: "30px", color: "#EFB38E" }} />
-                    {doc.name}
-                </div>
-            </Link>
-            ))
-            }
+           
+        <Content doc_number={1} rs={rs1} />
+        <Content doc_number={2} rs={rs2} />
+        <Content doc_number={3} rs={rs3} />
+        <Content doc_number={4} rs={rs4} />
         </div>
     </>
 }
+
+
+
+const Content = ({doc_number , rs} : any) => {
+    const [show, setShow] = useState<any>(false);
+
+    return  <>
+    
+    <div className='sub-nav-bar-item-content-folder-course' onClick={() => setShow(!show)}>
+            <Note style={{ width: "30px", height: "30px", color: "#EFB38E" }} />
+            Phụ lục {doc_number}
+        </div>
+    <div className={show ? 'nav-ok nav-is-show' : 'nav-ok nav-is-hidden'}>
+    {rs.map((doc: any) => (
+        <>
+        <Link key={doc.id} to={`/sub-menu-${doc.submenu}/detail-view/${doc.id}`}>
+        <div className='sub-nav-bar-item-content-folder-course'>
+            <Article style={{ width: "30px", height: "30px", color: "#EFB38E" }} />
+            {doc.name}
+        </div>
+    </Link>
+        </>
+    ))
+    }
+    </div>
+    </>
+}
+
 
 export default LeftSubNavBar
