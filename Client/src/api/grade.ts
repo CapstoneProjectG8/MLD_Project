ff537da1c8a4deabe81ec9d8e3ae57b17071e23d
Sull1298 <111106045+Sull1298@users.noreply.github.com>
2024-05-09 21:01:25 +0700

    merge code

--- Client/src/api/grade.ts
+++ Client/src/api/grade.ts
@@ -2,8 +2,6 @@ import { config } from "process";
 import { base_url } from "../utils/baseUrl"
 import axios from "axios";
 
-
-//chưa đổi
 export const apiGetGrade = async () => {
     const result = await axios.get(`${base_url}Grade`);
     if (result)
